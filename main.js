3e48991acc9ff5a70887edbdba92e874625872cf
Silvestre Luna <slvtrluna@gmail.com>
2019-03-25 18:02:18 -0500

    wip

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 const students = [];
+
+const newStudet = [];
 const expel = [];
 
 
@@ -9,7 +11,8 @@ const btnEvent = () => {
     document.getElementById('sortBtn').addEventListener('click', createStudentCard);
     document.getElementById('sortBtn').addEventListener('click', buildStudent);
     document.addEventListener('click', targetBtn);
-    //document.addEventListener('click', removeStd);
+    document.addEventListener('click', removeStudent);
+    document.addEventListener('click', clearValue);
 
 
 
@@ -48,19 +51,36 @@ const list = () => {
 const buildStudent = () => {
 
     let studentCard = '';
+    let idCounter = 1;
     students.forEach((student) => {
-        studentCard += `<div class="col-sm-4">`;
+       
+        studentCard += `<div id="card${idCounter}"class="col-sm-4">`;
         studentCard += `<div class="card">`;
         studentCard += `<h5 class="card-title">${student.name}</h5>`;
         studentCard += `<p>${student.house}</p>`;
         studentCard += `<button id="expelBtn" type="button">Expel</button>`;
         studentCard += `</div>`;
         studentCard += `</div>`;
+        idCounter++;
+        
     });
     printToDom('studentCard', studentCard);
 
 
+
 };
+
+// This will clear the input field after Sort button is clicked.
+const clearValue = () => {
+    document.getElementById('inputName').value = '';
+};
+
+
+// this prevents will prevent the page from refreshing
+// when enter key is clicked when input box is selected.
+$("form").bind("keydown", (e) => {
+    if (e.keyCode === 13) return false;
+});
 
 
 
@@ -72,6 +92,11 @@ const targetBtn = (e) => {
         expel.push(students);
          console.log(expel);
     };
+};
+
+
+const removeStudent = (e) => {
+ 
 };
 
 
